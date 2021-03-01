04c75170fcfa787188f26439f633cc230c0b3d14
Kid-Guru <birkov.alexandr@gmail.com>
2021-03-01 17:47:35 +0400

    webpack config devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const { NormalModuleReplacementPlugin } = require('webpack');
-// const { LoaderOptionsPlugin } = require('webpack');
 
 module.exports = {
   // context: path.resolve(__dirname, 'src'),
   mode: process.env.NODE_ENV || 'development',
-  entry: {
-    main: './src/index.js',
-  },
+  // entry: {
+  //   main: './src/index.js',
+  // },
+  entry: ['webpack/hot/dev-server', './src/index.js'],
   output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
   watchOptions: {
     poll: true,
     ignored: /node_modules/,
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './src'),
+    // contentBase: path.resolve(__dirname, './dist/'),
+    watchContentBase: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 4300,
+    inline: true,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/,
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       // {
       //   test: /\.js$/,
@@ -35,13 +55,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'RSS reader',
+      favicon: './src/images/favicon.png',
       template: './src/template.html',
     }),
     new CleanWebpackPlugin(),
-    // new NormalModuleReplacementPlugin(
-    //   /yup\/lib\/locale\.js/,
-    //   path.resolve(process.cwd(), './src/utils/validate.js'),
-    // ),
-
+    // new HotModuleReplacementPlugin(),
   ],
 };
